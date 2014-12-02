d3b6c180ce9696551df64ca91f83ff34711ebf95
ash47 <ash.fourtyseven@gmail.com>
2014-12-03 02:48:01 +1100

    Moved stuff, implemented token auth

---
+++ www/sql.php
@@ -0,0 +1,24 @@
+<?php
+// It is seperated like this because we rarely use MYSQL, so only make a connection if needed!
+
+$mysqli = null;
+
+// Ensures mysql is connected properly
+function mysqlConnect() {
+    // Use global mysqli
+    global $mysqli;
+
+    // MYSQL Settings
+    $dbhost = '127.0.0.1:3306';
+    $dbuser = 'root';
+    $dbpass = '3h(AEefAp~6RTkG';
+    $dbname = 'dotahost';
+
+    if($mysqli == null) {
+        // Attempt to conect
+        $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Failed to connect to mysql.');
+    }
+
+    return $mysqli;
+}
+?>
