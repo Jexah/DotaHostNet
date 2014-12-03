bc99ce5453e0ef0649d9480a53eb78b5a06f4f64
Jeremy Short <ijexah@gmail.com>
2014-12-03 04:27:28 -0800

    Made validate.php only accessible from the server

--- validate.php
+++ validate.php
@@ -1,40 +1,46 @@
 <?PHP
-$failString = 'get the fuck out of here';
+    $failString = 'get the fuck out of here';
+    if($_SERVER['REMOTE_ADDR'] != "108.61.169.195")
+    {
+        echo($failString);
+    }
+    else
+    {
+        include_once('sql.php');
+        $mysqli = mysqlConnect();
 
-include_once('sql.php');
-$mysqli = mysqlConnect();
+        // Filter for letters
+        if(preg_replace('/\D/', '', $_GET['steamID']) != $_GET['steamID']) {
+            echo $failString;
+            return;
+        }
 
-// Filter for letters
-if(preg_replace('/\D/', '', $_GET['steamID']) != $_GET['steamID']) {
-    echo $failString;
-    return;
-}
+        // Grab vars
+        $steamID = $mysqli->real_escape_string($_GET['steamID']);
+        $token = $mysqli->real_escape_string($_GET['token']);
 
-// Grab vars
-$steamID = $mysqli->real_escape_string($_GET['steamID']);
-$token = $mysqli->real_escape_string($_GET['token']);
+        // Create query
+        $query = "SELECT avatar, personaname, profileurl FROM steamUsers NATURAL JOIN sessionKeys WHERE sessionKeys.steamID = '".$steamID."' AND token = '".$token."'";
 
-// Create query
-$query = "SELECT avatar, personaname, profileurl FROM steamUsers NATURAL JOIN sessionKeys WHERE sessionKeys.steamID = '".$steamID."' AND token = '".$token."'";
+        // Run the query
+        $result = $mysqli->query($query);
+        $row = $result->fetch_row();
 
-// Run the query
-$result = $mysqli->query($query);
-$row = $result->fetch_row();
+        // Check if we found any
+        if($row) {
+                ?>
 
-// Check if we found any
-if($row && $_SERVER['REMOTE_ADDR'] == "127.0.0.1") {
-        ?>
+        {
+            steamID: <?PHP echo $steamID; ?>,
+            avatar: "<?PHP echo $row[0]; ?>",
+            personaname: "<?PHP echo addslashes($row[1]); ?>",
+            profileurl: "<?PHP echo $row[2]; ?>"
+        }
 
-{
-    steamID: <?PHP echo $steamID; ?>,
-    avatar: "<?PHP echo $row[0]; ?>",
-    personaname: "<?PHP echo addslashes($row[1]); ?>",
-    profileurl: "<?PHP echo $row[2]; ?>"
-}
-
-    <?PHP
-} else {
-    echo $failString;
-    return;
-}
+            <?PHP
+        } else {
+            echo $failString;
+            return;
+        }
+    }
 ?>
